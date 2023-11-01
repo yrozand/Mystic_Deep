066711dc52436e448b99abaa9c445cdc3ecdeea9
Yanis Rozand <yanis.rozand@gmail.com>
2023-11-01 20:15:39 +0100

    Premier commit!

---
+++ src/main/java/com/mystic/deep/MysticDeep.java
@@ -0,0 +1,12 @@
+package com.mystic.deep;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MysticDeep implements ModInitializer{
+
+    @Override
+    public void onInitialize() {
+        
+    }
+
+}
